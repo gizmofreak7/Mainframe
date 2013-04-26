8ba77a16e9ef2f286a474b0cf08ed4658f7eec4f
rzweers <rzweers@A205-11.ictemmeloord.nl>
2013-04-26 11:07:53 +0200

    Added extra test variables for upload test.

--- Front-end Webshop/index.php
+++ Front-end Webshop/index.php
@@ -8,10 +8,13 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        
     </head>
     <body>
-        
+        <form id="add" method="post" action='<?php echo $_SERVER["PHP_SELF"];?>'>
+            <input type="text" id="naam" value="">
+            <textarea id="beschrijving"></textarea>
+            <input type="text" id="prijs" value="">          
+        </form>
     </body>
 </html>
