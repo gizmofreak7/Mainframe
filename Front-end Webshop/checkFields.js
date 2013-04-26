c017d90f0c14dbe22de4f74de05ec2d76209403d
rzweers <rzweers@A205-11.ictemmeloord.nl>
2013-04-26 11:20:25 +0200

    Added fields checker for the forms

---
+++ Front-end Webshop/checkFields.js
@@ -0,0 +1,14 @@
+//checkFields.js
+//Checks the fields in a form, with the class 'check'.
+
+$(document).ready()({
+
+    
+    
+    
+    
+    
+    
+    
+    
+});
--- Front-end Webshop/index.php
+++ Front-end Webshop/index.php
@@ -1,20 +1,20 @@
 <?php
-
+    session_start();
 
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <link rel="stylesheet" type="text/css" href="css/page.css">
+        <script type="text/javascript" src="js/checkFields.js"></script>
     </head>
     <body>
         <form id="add" method="post" action='<?php echo $_SERVER["PHP_SELF"];?>'>
-            <input type="text" id="naam" value="">
-            <textarea id="beschrijving"></textarea>
-            <input type="text" id="prijs" value="">          
+            <input type="text" id="naam" class="check" value="">
+            <textarea id="beschrijving" class="check"></textarea>
+            <input type="text" id="prijs" class="check" value="">          
         </form>
     </body>
 </html>
