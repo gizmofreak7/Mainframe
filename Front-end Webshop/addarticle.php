0938c4832ac1bbf4154fa342efeb6abb592c9c1c
rzweers <rzweers@A205-11.ictemmeloord.nl>
2013-04-26 11:31:52 +0200

    Updated folder structure for incoming files.

--- Front-end Webshop/addarticle.php
+++ Front-end Webshop/addarticle.php
@@ -8,13 +8,12 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="css/page.css">
-        <script type="text/javascript" src="js/checkFields.js"></script>
     </head>
     <body>
         <form id="add" method="post" action='<?php echo $_SERVER["PHP_SELF"];?>'>
-            <input type="text" id="naam" class="check" value="">
-            <textarea id="beschrijving" class="check"></textarea>
-            <input type="text" id="prijs" class="check" value="">          
+            <input type="text" id="naam" class="check" required value="">
+            <textarea id="beschrijving" class="check" required></textarea>
+            <input type="text" id="prijs" class="check" required value="">          
         </form>
     </body>
 </html>
